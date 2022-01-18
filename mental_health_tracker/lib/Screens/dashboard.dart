d8044d5a7ddcbb619622fa4dad86911f9978b93a
Yash <yashbudhe25@gmail.com>
2022-01-19 00:06:46 +0530

    DrawerUIDone

--- mental_health_tracker/lib/Screens/dashboard.dart
+++ mental_health_tracker/lib/Screens/dashboard.dart
@@ -4,7 +4,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mental_health_tracker/Constants/colors.dart';
 import 'package:mental_health_tracker/models/user_model.dart';
-import 'package:mental_health_tracker/widgets/custom_button.dart';
 import 'package:mental_health_tracker/widgets/rect_button.dart';
 
 class DashBoardPage extends StatefulWidget {
@@ -15,10 +14,8 @@ class DashBoardPage extends StatefulWidget {
 }
 
 class _DashBoardPageState extends State<DashBoardPage> {
-
   //Global Scaffold key
-  GlobalKey<ScaffoldState> _scaffoldkey = GlobalKey<ScaffoldState>(); 
-
+  GlobalKey<ScaffoldState> _scaffoldkey = GlobalKey<ScaffoldState>();
 
   //fetching the current user from firestore
   User? user = FirebaseAuth.instance.currentUser;
@@ -43,7 +40,56 @@ class _DashBoardPageState extends State<DashBoardPage> {
       child: Scaffold(
         key: _scaffoldkey,
         backgroundColor: Color(dashbg),
-        drawer: Drawer(),
+        drawer: ClipRRect(
+          borderRadius: BorderRadius.only(topRight: Radius.circular(20.0), bottomRight: Radius.circular(20.0)),
+          child: Drawer(
+            child: ListView(
+              padding: EdgeInsets.all(0),
+              children: <Widget>[
+                UserAccountsDrawerHeader(
+                  accountEmail: Text(loggedInUser.email.toString()),
+                  accountName: Text(loggedInUser.name.toString()),
+                  currentAccountPicture: CircleAvatar(
+                    child: Text(loggedInUser.name.toString().substring(0,2).toUpperCase()),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(Icons.home),
+                  title: Text("Home"),
+                  onTap: () {
+                    Navigator.pushNamed(context, '/dashboard');
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.info),
+                  title: Text("About Us"),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: Icon(Icons.rate_review),
+                  title: Text("Rate and Review"),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: Icon(Icons.flag),
+                  title: Text("Privacy Policy"),
+                  onTap: () {},
+                ),
+                Divider(
+                  thickness: 2.0,
+                ),
+                ListTile(
+                  leading: Icon(Icons.logout_rounded),
+                  title: Text("Log-Out"),
+                  onTap: () {
+                    DeleteCache.deleteKey("cache",
+                        Navigator.of(context).pushReplacementNamed('/login'));
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
         body: Column(
           children: [
             Container(
@@ -57,29 +103,33 @@ class _DashBoardPageState extends State<DashBoardPage> {
                   RichText(
                     text: TextSpan(
                       text: 'Hey ${loggedInUser.name}',
-                      style: const TextStyle(color: Colors.black, fontSize: 32.0, fontFamily: 'Farro', fontWeight: FontWeight.w600),
+                      style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 32.0,
+                          fontFamily: 'Farro',
+                          fontWeight: FontWeight.w600),
                       children: const <TextSpan>[
                         TextSpan(
-                            text: '\nYour Report',
-                            style: TextStyle(color: Colors.black87, fontSize: 32.0, fontFamily: 'Farro', fontWeight: FontWeight.w600),)
+                          text: '\nYour Report',
+                          style: TextStyle(
+                              color: Colors.black87,
+                              fontSize: 32.0,
+                              fontFamily: 'Farro',
+                              fontWeight: FontWeight.w600),
+                        )
                       ],
                     ),
                   ),
-                  RectButton(onPressed: (){setState(() {
-                    _scaffoldkey.currentState?.openDrawer();
-                  });}, ic: Icon(Icons.menu)),
+                  RectButton(
+                      onPressed: () {
+                        setState(() {
+                          _scaffoldkey.currentState?.openDrawer();
+                        });
+                      },
+                      ic: Icon(Icons.menu)),
                 ],
               ),
             ),
-            Center(
-                child: CustomButton(
-                    buttonText: "Logout",
-                    onPressed: () {
-                      DeleteCache.deleteKey("cache",
-                          Navigator.of(context).pushReplacementNamed('/login'));
-                    },
-                    height: 60,
-                    width: 80)),
           ],
         ),
       ),
