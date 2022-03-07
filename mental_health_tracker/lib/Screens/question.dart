b0cfd7146f95614a656cc997afb85f954f4c0c7c
Yash <yashbudhe25@gmail.com>
2022-03-07 12:36:18 +0530

    questionPageUpdated

--- mental_health_tracker/lib/Screens/question.dart
+++ mental_health_tracker/lib/Screens/question.dart
@@ -1,12 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:mental_health_tracker/Services/database.dart';
 import 'package:mental_health_tracker/models/question_model.dart';
-import 'package:mental_health_tracker/widgets/question_widget.dart';
 
 class QuestionPage extends StatefulWidget {
-  const QuestionPage({ Key? key }) : super(key: key);
-
+  const QuestionPage({Key? key, required this.questionIn}) : super(key: key);
+  final num questionIn;
 
   @override
   _QuestionPageState createState() => _QuestionPageState();
@@ -15,28 +13,113 @@ class QuestionPage extends StatefulWidget {
 class _QuestionPageState extends State<QuestionPage> {
   QuestionModel questions = QuestionModel();
 
-  String quetionindex = "1";
-  
-
-  
-
   @override
   void initState() {
     super.initState();
     FirebaseFirestore.instance
         .collection("questions")
-        .doc(quetionindex)
+        .doc(widget.questionIn.toString())
         .get()
         .then((value) {
-      this.questions = QuestionModel.fromMap(value.data());
+      questions = QuestionModel.fromMap(value.data());
       setState(() {});
     });
   }
-  
+
+  double _rating = 1.0;
+  num score = 0;
+
+  void _onChanged(double value) {
+    setState(() {
+      _rating = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    num questionindex = widget.questionIn;
     return Scaffold(
-      body: QuestionBtn(questionModel: this.questions,),
+        body: Padding(
+      padding: const EdgeInsets.fromLTRB(10, 26, 10, 10),
+      child: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.amber[200],
+              borderRadius: BorderRadius.circular(20),
+            ),
+            height: 150,
+            width: 380,
+            child: Text(
+              "${questions.question}",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontFamily: "Farro",
+              ),
+              softWrap: true,
+            ),
+          ),
+          Image.network(questions.imgurl.toString(),
+              width: 400.0, height: 250.0, errorBuilder: (BuildContext context,
+                  Object exception, StackTrace? stackTrace) {
+            return const CircularProgressIndicator();
+          }),
+          // Buttons From Here
+          Container(
+            child: Column(
+              children: [
+                questionBtn(
+                  val: questions.option1txt,
+                  fun: () => {
+                    score = questions.option1pt!,
+                  },
+                ),
+                questionBtn(
+                  val: questions.option2txt,
+                  fun: () => {
+                    score = questions.option2pt!,
+                  },
+                ),
+                questionBtn(
+                  val: questions.option3txt,
+                  fun: () => {
+                    score = questions.option3pt!,
+                  },
+                ),
+                questionBtn(
+                  val: questions.option4txt,
+                  fun: () => {
+                    score = questions.option4pt!,
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
+}
+
+// single btn layout
+class questionBtn extends StatelessWidget {
+  const questionBtn({Key? key, this.val, this.fun}) : super(key: key);
+  final String? val;
+  final VoidCallback? fun;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ButtonStyle(),
+      child: Text(
+        "$val",
+        style:
+            TextStyle(color: Colors.white, fontSize: 18.0, fontFamily: "Farro"),
+      ),
+      onPressed: fun,
     );
   }
 }
