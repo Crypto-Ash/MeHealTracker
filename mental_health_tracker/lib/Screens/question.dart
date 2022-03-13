ad8af5b703328a2d1c53d20164c02988cd7182ad
Yash <yashbudhe25@gmail.com>
2022-03-13 12:20:00 +0530

    questionpageUpdate

--- mental_health_tracker/lib/Screens/question.dart
+++ mental_health_tracker/lib/Screens/question.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:mental_health_tracker/models/question_model.dart';
-import 'package:mental_health_tracker/models/user_model.dart';
 
 final questionRef = FirebaseFirestore.instance.collection('questions');
 final userRef = FirebaseFirestore.instance.collection('users');
