ecd66008957c50826c6ca568f9adb84385a907d3
Yash <yashbudhe25@gmail.com>
2022-03-15 15:33:31 +0530

    SomeUIChanges

--- mental_health_tracker/lib/Screens/dashboard.dart
+++ mental_health_tracker/lib/Screens/dashboard.dart
@@ -10,6 +10,7 @@ import 'package:mental_health_tracker/models/user_model.dart';
 import 'package:mental_health_tracker/widgets/rect_button.dart';
 
 final userRef = FirebaseFirestore.instance.collection('users');
+
 class DashBoardPage extends StatefulWidget {
   const DashBoardPage({Key? key}) : super(key: key);
 
@@ -156,11 +157,11 @@ class _DashBoardPageState extends State<DashBoardPage> {
             ),
             //Detail-Card
             Padding(
-              padding: const EdgeInsets.only(top: 20.0),
+              padding: const EdgeInsets.only(top: 20.0, bottom: 10.0),
               child: Container(
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(45.0),
-                  color: const Color(cardbg),
+                  color: const Color(0xffc0dfd3),
                 ),
                 height: MediaQuery.of(context).size.height - 450.0,
                 width: MediaQuery.of(context).size.width - 50.0,
@@ -197,68 +198,120 @@ class _DashBoardPageState extends State<DashBoardPage> {
                           style: const TextStyle(
                               fontSize: 29.0,
                               fontFamily: 'Farro',
-                              color: Colors.white,
+                              color: Color(0xff6f7c6b),
                               fontWeight: FontWeight.w700),
+                        )),
+                    Positioned(
+                        left: 25.0,
+                        bottom: 39.0,
+                        child: Text(
+                          "Question Answerd : ${loggedInUser.questionans}/10",
+                          style: const TextStyle(
+                              fontSize: 22.0,
+                              fontFamily: 'Farro',
+                              color: Color(0xff6f7c6b),
+                              fontWeight: FontWeight.w500),
                         )),
                   ],
                 ),
               ),
             ),
-            ElevatedButton(
-              child: const Text("Questions"),
-              onPressed: () {
-                print("======>>> $_currentDate ");
-                if (UserSimplePreferences.getDate() == null) {
-                  setState(() {
-                    UserSimplePreferences.setDate(_currentDate);
-                  });
-                } else {
-                  if (calculateDifference(
-                          DateTime.parse(UserSimplePreferences.getDate()!)) == 0) {
-                            print("First If Loop");
-                    if (loggedInUser.questionans != "10") {
-                      print("2nd If Loop");
-                      setState(() {
-                        Navigator.push(context, MaterialPageRoute<void>(
-                          builder: (BuildContext context) {
-                            return QuestionPage(
-                              questionIn: int.parse(
-                                  loggedInUser.questionIndex.toString()),
-                              userid: loggedInUser.uid.toString(),
-                            );
-                          },
-                        ));
-                      });
-                    } // if-loop Ends
-                    else{
-                      print("I am Seting counter to 1");
-                      UserSimplePreferences.setCounter(1);
-                      Fluttertoast.showToast(msg: "You Have Completed Today's Questionner!!! \n\t Try Again Later...",textColor: Colors.white, fontSize: 18,toastLength: Toast.LENGTH_LONG, backgroundColor: const Color(0xffF59397));
-                    }
-                  }else{
-                    if(calculateDifference(
-                          DateTime.parse(UserSimplePreferences.getDate()!)) < 0){
-                            print("I am Next Day!!!!");
-                        UserSimplePreferences.setDate(_currentDate);
-                        UserSimplePreferences.setCounter(1);
-                        updater.update({
-                          "userinfo.questionans": 0.toString(),
-                        }); 
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0,top: 10.0),
+              child: SizedBox(
+                width: 280,
+                height: 100,
+                child: ElevatedButton(
+                    child: const Text(
+                      "Questions",
+                      style: TextStyle(
+                          fontFamily: "farro",
+                          fontSize: 25.0,
+                          color: Color(0xffd291ff)),
+                    ),
+                    onPressed: () {
+                      print("======>>> $_currentDate ");
+                      if (UserSimplePreferences.getDate() == null) {
                         setState(() {
-                        Navigator.push(context, MaterialPageRoute<void>(
-                          builder: (BuildContext context) {
-                            return QuestionPage(
-                              questionIn: int.parse(
-                                  loggedInUser.questionIndex.toString()),
-                              userid: loggedInUser.uid.toString(),
-                            );
-                          },
-                        ));
-                      });
-                    }
-                  }
-                }
-              },
+                          UserSimplePreferences.setDate(_currentDate);
+                        });
+                      } else {
+                        if (calculateDifference(DateTime.parse(
+                                UserSimplePreferences.getDate()!)) ==
+                            0) {
+                          print("First If Loop");
+                          if (loggedInUser.questionans != "10") {
+                            print("2nd If Loop");
+                            setState(() {
+                              Navigator.push(context, MaterialPageRoute<void>(
+                                builder: (BuildContext context) {
+                                  return QuestionPage(
+                                    questionIn: int.parse(
+                                        loggedInUser.questionIndex.toString()),
+                                    userid: loggedInUser.uid.toString(),
+                                  );
+                                },
+                              ));
+                            });
+                          } // if-loop Ends
+                          else {
+                            print("I am Seting counter to 1");
+                            UserSimplePreferences.setCounter(1);
+                            Fluttertoast.showToast(
+                                msg:
+                                    "You Have Completed Today's Questionner!!! \n\t Try Again Later...",
+                                textColor: Colors.white,
+                                fontSize: 18,
+                                toastLength: Toast.LENGTH_LONG,
+                                backgroundColor: const Color(0xffF59397));
+                          }
+                        } else {
+                          if (calculateDifference(DateTime.parse(
+                                  UserSimplePreferences.getDate()!)) <
+                              0) {
+                            print("I am Next Day!!!!");
+                            UserSimplePreferences.setDate(_currentDate);
+                            UserSimplePreferences.setCounter(1);
+                            updater.update({
+                              "userinfo.questionans": 0.toString(),
+                            });
+                            setState(() {
+                              Navigator.push(context, MaterialPageRoute<void>(
+                                builder: (BuildContext context) {
+                                  return QuestionPage(
+                                    questionIn: int.parse(
+                                        loggedInUser.questionIndex.toString()),
+                                    userid: loggedInUser.uid.toString(),
+                                  );
+                                },
+                              ));
+                            });
+                          }
+                        }
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                        elevation: 3,
+                        primary: const Color(0xfff2e7f5),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)))),
+              ),
+            ),
+            SizedBox(
+              width: 250,
+              height: 100,
+              child: ElevatedButton(child: const Text("Tasks", style: TextStyle(
+                fontFamily: "farro",
+                fontSize: 25.0,
+                color: Color(0xffd291ff),
+              ))
+              ,onPressed: (){},
+              style: ElevatedButton.styleFrom(
+                      elevation: 3,
+                      primary: const Color(0xfff2e7f5),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)))
+              ),
             )
           ],
         ),
